8f78375d96539ccd0369b22e41274c1748bfbfcb
AIN CORP <andykumala777@gmail.com>
2024-03-09 16:37:48 +0700

    db

---
+++ crmptain (1).sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:8111
+-- Generation Time: Feb 05, 2024 at 07:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crmptain`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customerque`
+--
+-- Error reading structure for table crmptain.customerque: #1932 - Table &#039;crmptain.customerque&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.customerque: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`customerque`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_agent`
+--
+-- Error reading structure for table crmptain.history_agent: #1932 - Table &#039;crmptain.history_agent&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.history_agent: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`history_agent`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+-- Error reading structure for table crmptain.level: #1932 - Table &#039;crmptain.level&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.level: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`level`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+-- Error reading structure for table crmptain.product: #1932 - Table &#039;crmptain.product&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.product: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`product`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session_logs`
+--
+-- Error reading structure for table crmptain.session_logs: #1932 - Table &#039;crmptain.session_logs&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.session_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`session_logs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stat_agent`
+--
+-- Error reading structure for table crmptain.stat_agent: #1932 - Table &#039;crmptain.stat_agent&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.stat_agent: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`stat_agent`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stat_leader`
+--
+-- Error reading structure for table crmptain.stat_leader: #1932 - Table &#039;crmptain.stat_leader&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.stat_leader: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`stat_leader`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_agent`
+--
+-- Error reading structure for table crmptain.tb_agent: #1932 - Table &#039;crmptain.tb_agent&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_agent: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_agent`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_callback`
+--
+-- Error reading structure for table crmptain.tb_callback: #1932 - Table &#039;crmptain.tb_callback&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_callback: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_callback`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_customer`
+--
+-- Error reading structure for table crmptain.tb_customer: #1932 - Table &#039;crmptain.tb_customer&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_customer: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_customer`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_delivery`
+--
+-- Error reading structure for table crmptain.tb_delivery: #1932 - Table &#039;crmptain.tb_delivery&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_delivery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_delivery`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_disclaimer`
+--
+-- Error reading structure for table crmptain.tb_disclaimer: #1932 - Table &#039;crmptain.tb_disclaimer&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_disclaimer: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_disclaimer`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_finance`
+--
+-- Error reading structure for table crmptain.tb_finance: #1932 - Table &#039;crmptain.tb_finance&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_finance: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_finance`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_handling`
+--
+-- Error reading structure for table crmptain.tb_handling: #1932 - Table &#039;crmptain.tb_handling&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_handling: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_handling`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_image`
+--
+-- Error reading structure for table crmptain.tb_image: #1932 - Table &#039;crmptain.tb_image&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_image: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_image`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_leader`
+--
+-- Error reading structure for table crmptain.tb_leader: #1932 - Table &#039;crmptain.tb_leader&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_leader: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_leader`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_manager`
+--
+-- Error reading structure for table crmptain.tb_manager: #1932 - Table &#039;crmptain.tb_manager&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_manager: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_manager`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_notconnect`
+--
+-- Error reading structure for table crmptain.tb_notconnect: #1932 - Table &#039;crmptain.tb_notconnect&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_notconnect: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_notconnect`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_opstatus`
+--
+-- Error reading structure for table crmptain.tb_opstatus: #1932 - Table &#039;crmptain.tb_opstatus&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_opstatus: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_opstatus`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_outstanding`
+--
+-- Error reading structure for table crmptain.tb_outstanding: #1932 - Table &#039;crmptain.tb_outstanding&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_outstanding: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_outstanding`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_procces`
+--
+-- Error reading structure for table crmptain.tb_procces: #1932 - Table &#039;crmptain.tb_procces&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_procces: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_procces`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_quetype`
+--
+-- Error reading structure for table crmptain.tb_quetype: #1932 - Table &#039;crmptain.tb_quetype&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_quetype: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_quetype`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_queue`
+--
+-- Error reading structure for table crmptain.tb_queue: #1932 - Table &#039;crmptain.tb_queue&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_queue: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_queue`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_spam`
+--
+-- Error reading structure for table crmptain.tb_spam: #1932 - Table &#039;crmptain.tb_spam&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_spam: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_spam`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_stat_history`
+--
+-- Error reading structure for table crmptain.tb_stat_history: #1932 - Table &#039;crmptain.tb_stat_history&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_stat_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_stat_history`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_stat_history_leader`
+--
+-- Error reading structure for table crmptain.tb_stat_history_leader: #1932 - Table &#039;crmptain.tb_stat_history_leader&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_stat_history_leader: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_stat_history_leader`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_trash`
+--
+-- Error reading structure for table crmptain.tb_trash: #1932 - Table &#039;crmptain.tb_trash&#039; doesn&#039;t exist in engine
+-- Error reading data for table crmptain.tb_trash: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crmptain`.`tb_trash`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
